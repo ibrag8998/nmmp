844acc60191d7d02eb730f665f010f1a2b8b4c86
ibrag8998 <ibragdzh@gmail.com>
2020-05-07 21:21:22 +0300

    level up

--- simple_iters/sol.py
+++ simple_iters/sol.py
@@ -1,129 +1,134 @@
 #!/usr/bin/env python
+from sympy import symbols, Eq, solve
 import numpy as np
-from sympy import (
-    symbols,
-    Eq,
-    solve
-)
-from colorama import (
-    Fore,
-    Style
-)
+from numpy.linalg import norm
 
 
-def logg(*args, **kwargs):
-    print(f'\n[ {Fore.BLUE}{Style.BRIGHT}LOG{Style.RESET_ALL} ] ', end='')
-    print(*args, **kwargs)
+def build_eq(coeffs, free):
+    return Eq(
+        sum([coeffs[i] * x[i+1] for i in range(4)]),
+        free
+    )
 
 
-def mkiter(last):
+def prep(v):
+    return round(float(v), 5)
+
+
+def build_AB():
+    A = np.array([])
+    B = np.array([])
+
+    for i in sol:
+        coeffs = sol[i].as_expr().as_coefficients_dict()
+        x_coeffs = [prep(coeffs.get(x[i], 0)) for i in x]
+
+        A = np.append(A, x_coeffs)
+        B = np.append(B, prep(coeffs.get(1, 0)))
+
+    A = A.reshape((4, 4))
+
+    return A, B
+
+
+def mkiter():
     return np.array([
-        round(
-            float(sol[i].subs(
-                [(x[j], last[j-1]) for j in hrange(3)]
-            )),
-            5
-        ) for i in hrange(3)
+        prep(
+            sol[i].subs(
+                [(x[j], xmx[-1][j-1]) for j in range(1, 5)]
+            )
+        ) for i in range(1, 5)
     ])
 
 
-def hrange(n):
-    """ Human range """
-    return range(1, n+1)
+def intro():
+    print('Матрица альфа:')
+    print(A, end='\n\n')
+
+    print('Матрица бета:')
+    print(B, end='\n\n')
+
+    q = norm(A, 1)
+    print('Норма матрицы альфа:')
+    print(q, end='\n\n')
+
+    if q >= 1:
+        print('Норма больше единицы, можно не решать')
+        exit()
+
+    # matrix of x -s
+    xmx.append(mkiter())
+
+    print('x(0) это матрица бета, x(1) равно:')
+    print(xmx[1], end='\n\n')
+
+    diff = xmx[1] - xmx[0]
+    print('Разность x(1) - x(0):')
+    print(diff, end='\n\n')
+
+    diff_norm = prep(norm(diff, 1))
+    print('Норма разности ||x(1) - x(0)||:')
+    print(diff_norm, end='\n\n')
 
 
-# eps
+def mkiters():
+    i = 0
+    while prep(norm(xmx[-1] - xmx[-2], 1)) > eps:
+        i += 1
+
+        print('Итерация', i)
+        print('===========')
+
+        xmx.append(mkiter())
+        print(f'x({i}):')
+        print(xmx[-1], end='\n\n')
+
+        diff_norm = prep(norm(xmx[-1] - xmx[-2], 1))
+        print(f'||x({i}) - x({i-1})||:')
+        print(diff_norm, end='\n\n')
+
+
+def main():
+    intro()
+
+    input('Нажмите Enter чтоб начать итерирование')
+    print('='*21)
+    print(' НАЧАЛО ИТЕРИРОВАНИЯ ')
+    print('='*21, end='\n\n\n')
+
+    mkiters()
+
+    print('Разность нормы меньше eps, конец итерирования')
+
+    print('=============')
+    print('=== ОТВЕТ ===')
+    print('=============', end='\n\n')
+
+    print('\n'.join([f'x{i+1} = {xmx[-1][i]}' for i in range(4)]))
+
+
 eps = .01
-# symbols x1, x2, x3
+
 x = {
-    i: symbols(f'x{i}') for i in hrange(3)
+    i: symbols(f'x{i}') for i in range(1, 5)
 }
-# expessions
-expr = {
-    1: 175.1*x[1] + 28*x[2] + 47*x[3],
-    2: x[1] + 18.5*x[2] + 8.01*x[3],
-    3: 6.1*x[1] + 12.002*x[2] + 48*x[3]
-}
-# equalities
+
 eq = {
-    1: Eq(expr[1], 269.1),
-    2: Eq(expr[2], 17.02),
-    3: Eq(expr[3], 102.1)
+    1: build_eq((-.77, -.04, .21, -.18), -1.24),
+    2: build_eq((.25, -1.23, .16, -.09), 1.12),
+    3: build_eq((-.21, .16, .8, -.13), 2.56),
+    4: build_eq((.15, -.31, .06, 1.12), -.77),
 }
-# solutions of eq[i] for x[i]
+
 sol = {
-    i: solve(eq[i], x[i])[0] for i in hrange(3)
+    i: solve(eq[i], x[i])[0] for i in range(1, 5)
 }
 
-print(solve(
-    (eq[1], eq[2], eq[3]),
-    (x[1], x[2], x[3])
-))
+A, B = build_AB()
 
-# extracting coeffs
-x_coeffs = []
-frees = []
-for i in sol:
-    tmp_expr = sol[i]
-    coeffs = tmp_expr.as_coefficients_dict()
-    x_coeffs.append(
-        [float(coeffs.get(x[i], 0)) for i in x]
-    )
-    frees.append(float(coeffs.get(1, 0)))
-
-# matrix of x coeffs
-A = np.around(np.array(x_coeffs), 5)
-# matrix of free coeffs
-B = np.around(np.array(frees), 5)
-
-logg('A:')
-print(A)
-logg('B:')
-print(B)
-
-# A norm
-A_norm = np.linalg.norm(A, 1)
-logg('||A||:', A_norm)
-
-if A_norm >= 1: # needed condition
-    exit()
-
-# x matrix
 xmx = [B]
 
-xmx.append(mkiter(xmx[-1]))
 
-logg('xmx[0]:')
-print(xmx[0])
-
-logg('xmx[1]:')
-print(xmx[1])
-
-diff = np.around(xmx[1] - xmx[0], 5)
-logg('xmx[1] - xmx[0]')
-print(diff)
-
-diff_norm = np.linalg.norm(diff, 1)
-logg('||xmx[1] - xmx[0]||:', diff_norm)
-
-m = np.log((eps * (1 - A_norm)) / diff_norm) / np.log(A_norm)
-logg('m:', m)
-
-for i in range(2, np.int_(m) + 2):
-    print('\n\n\n')
-    logg('iteration number', i)
-    xmx.append(mkiter(xmx[-1]))
-
-    logg(f'xmx[{i-1}]:')
-    print(xmx[i-1])
-
-    logg(f'xmx[{i}]:')
-    print(xmx[i])
-
-    diff = np.around(xmx[i] - xmx[i-1], 5)
-    logg(f'xmx[{i}] - xmx[{i-1}]')
-    print(diff)
-
-    diff_norm = np.linalg.norm(diff, 1)
-    logg(f'||xmx[{i}] - xmx[{i-1}]||:', diff_norm)
+if __name__ == '__main__':
+    main()
 
